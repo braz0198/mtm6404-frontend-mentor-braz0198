249a9bc3457c5d52a4b70065c04ed7650d3abb0d
Jackson Brazeau <braz0198@algonquinlive.com>
2023-11-24 15:07:20 -0500

    Fixing css links, formatting code

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const $container = document.querySelector('.container')
-const $openMenu = document.querySelector('.hamburger-icon')
-const $closeMenu = document.querySelector('.cross-icon')
-const $nav = document.querySelector('nav')
+const $container = document.querySelector(".container");
+const $openMenu = document.querySelector(".hamburger-icon");
+const $closeMenu = document.querySelector(".cross-icon");
+const $nav = document.querySelector("nav");
 
-$openMenu.addEventListener('click', function ShowMenu(){
-    $nav.classList.toggle('hide')
-    $closeMenu.classList.toggle('hide')
-    $openMenu.classList.toggle('hide')
-})
+$openMenu.addEventListener("click", function ShowMenu() {
+  $nav.classList.toggle("hide");
+  $closeMenu.classList.toggle("hide");
+  $openMenu.classList.toggle("hide");
+});
 
-$closeMenu.addEventListener('click', function CloseMenu(){
-    $nav.classList.toggle('hide')
-    $openMenu.classList.toggle('hide')
-    $closeMenu.classList.toggle('hide')
-} )
-
-
-
-
-
-    
-
-
-
+$closeMenu.addEventListener("click", function CloseMenu() {
+  $nav.classList.toggle("hide");
+  $openMenu.classList.toggle("hide");
+  $closeMenu.classList.toggle("hide");
+});
